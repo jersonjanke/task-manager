0a217420c3b54ce3157c64445b88886ce19d9a29
Jerson Janke <jersonjanke@gmail.com>
2017-12-06 20:35:47 -0200

    Login CSS

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import {  Router, ActivatedRoute } from '@angular/router';
-import { CacheModule, CacheLoader, CacheStaticLoader } from '@ngx-cache/core';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -10,29 +9,31 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
+  email = "";
+  password = "";
   login: any = null;
+  isLoading = false;
+
   constructor(private http: Http,
               private router: Router, 
               private route: ActivatedRoute) { }
 
   ngOnInit() {}
 
-  generatorCacheLogin() {
-
-  }
-
   getLogin(form) {
+    this.isLoading = true;
     this.http.post('http://localhost:3000/users-login', form.value)
     .map((res: Response) => res.json())
-    .subscribe(res => {      
-      console.log(res);
-      this.login = res.login;      
+    .subscribe(res => {            
+      this.login = res.login;         
       if(this.login) {
-        this.router.navigate(['../task-list'], { relativeTo: this.route });
-        //document.cookie = `user=${res.user}`;
+        this.router.navigate(['../task-list'], { relativeTo: this.route });        
         localStorage.setItem('user', res.user);
+      } else {
+        this.email = "";
+        this.password = "";
       }
+      this.isLoading = false;   
     });
   }
 }
